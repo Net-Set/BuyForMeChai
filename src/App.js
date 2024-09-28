b0b44b0854e0f2de8c1345bd7aef2dceb7e660c3
Net-Set <somilsomil25@gmail.com>
2024-09-28 19:44:33 +0530

    28-09-24

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
       <Router>
         <Navbar expand="lg" className={isDarkMode ? 'bg-dark' : 'bg-body-tertiary'}>
           <Container>
-            <Navbar.Brand as={Link} to="/" className={isDarkMode ? 'text-light' : 'text-dark'}>BuyChai</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/" className={isDarkMode ? 'text-light' : 'text-dark'}>BuyForMeChai</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
